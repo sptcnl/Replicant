f70a8f9e288dfac6366c85037f33efeb3dcdfce9
sptcnl <sptcnl@gmail.com>
2025-06-17 20:39:17 +0900

    🎉 Initial commit: backend project setup

---
+++ backend/apps/accounts/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    GENDER_CHOICE = (
+            ('M', 'Man'),
+            ('W', 'Woman'),
+            ('O', 'Other'),
+        )
+    username = models.CharField(max_length=10, unique=True)
+    email = models.EmailField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICE, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
