2ff1bb45501331c2e008739400210a9ec5efd558
sptcnl <sptcnl@gmail.com>
2025-06-18 00:09:25 +0900

    ✨ Feat: 회원가입, 로그인, 토큰 재발급 추가

--- backend/apps/accounts/views.py
+++ backend/apps/accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import AllowAny
+from .serializers import (
+                            SignupSerializer
+                        )
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@permission_classes([AllowAny])
+class SignupView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = SignupSerializer
+
+    def perform_create(self, serializer):
+        try:
+            # 객체 저장 시도
+            serializer.save()
+        except Exception as e:
+            raise ValidationError({"detail": f"An error occurred: {str(e)}"})
