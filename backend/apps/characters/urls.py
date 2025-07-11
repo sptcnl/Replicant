a2b5632108fa8384e06f94c9b93605340f7ca58b
sptcnl <sptcnl@gmail.com>
2025-07-12 01:33:44 +0900

    ✨ Feat: 캐릭터 상세(조회, 수정, 삭제) API 완료

--- backend/apps/characters/urls.py
+++ backend/apps/characters/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.CharacterLCView.as_view()),
+    path("<str:pk>/", views.CharacterRUDView.as_view()),
     path("tag/", views.tag_list),
 ]
--- backend/apps/characters/views.py
+++ backend/apps/characters/views.py
@@ -6,6 +6,12 @@ from rest_framework.permissions import AllowAny
 from .models import Character, Tag
 from .serializers import CharacterSerializer, TagNameListSerializer
 
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+
 class CharacterLCView(generics.ListCreateAPIView):
     queryset = Character.objects.all()
     serializer_class = CharacterSerializer
@@ -28,3 +34,18 @@ def tag_list(request):
     tag_names = Tag.objects.values_list('name', flat=True)
     serializer = TagNameListSerializer(tag_names)
     return Response(serializer.data)
+
+
+class CharacterRUDView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CharacterSerializer
+
+    def get_queryset(self):
+        if self.request.method == 'GET':
+            return Character.objects.all()
+        # PUT, PATCH, DELETE 요청일 때
+        return Character.objects.filter(user=self.request.user)
+    
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [permissions.AllowAny()]
+        return [IsOwner()]
