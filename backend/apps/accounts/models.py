fc5d35e13887fb1bc8733ef2edf0146923e9ab4c
sptcnl <sptcnl@gmail.com>
2025-07-18 03:51:52 +0900

    🗃️ DB: 프로필 필드 null blank 허용

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
             ('O', 'Other'),
         )
     username = models.CharField(max_length=10, unique=True)
-    profile_img = models.ImageField()
+    profile_img = models.ImageField(blank=True, null=True)
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICE, blank=True, null=True)
 
